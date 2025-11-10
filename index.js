989d03a1ab6c52b1a5a22239db660141c1372b5f
shemul345 <shemulchowdhury7684@gmail.com>
2025-11-10 17:41:03 +0600

    successfully db connect to client and server side with vehicles collection

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
+
+const uri = "mongodb+srv://travelEase_db:gZy9GZ9Etx9iciBT@cluster0.svjtwrm.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('travelEase_db')
+    const vehiclesCollection = db.collection('vehicles')
+
+    app.get('/vehicles', async(req, res) => {
+      const result = await vehiclesCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
